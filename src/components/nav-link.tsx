b7ad1799c6f787b4c21d40871b3cd181122084fd
Renan Borges <renanvcb@gmail.com>
2024-04-05 18:03:30 -0300

    Feat: navlink props improved

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,10 +1,12 @@
-interface NavLinkProps {
+import { ComponentProps } from "react";
+
+interface NavLinkProps extends ComponentProps<"a"> {
   children: string;
 }
 
-export function NavLink({ children }: NavLinkProps) {
+export function NavLink({ children, ...rest }: NavLinkProps) {
   return (
-    <a className="font-medium text-sm text-zinc-300" href="">
+    <a className="font-medium text-sm text-zinc-300" {...rest}>
       {children}
     </a>
   );
