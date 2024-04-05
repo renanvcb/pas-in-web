de4fb959cb338771273c998ef5a2dd972f6b0b8f
Renan Borges <renanvcb@gmail.com>
2024-04-05 19:30:11 -0300

    Feat: table row component created

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -10,6 +10,7 @@ import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
+import { TableRow } from "./table/table-row";
 
 export function AttendeeList() {
   return (
@@ -44,7 +45,7 @@ export function AttendeeList() {
         <tbody>
           {Array.from({ length: 8 }).map((_, i) => {
             return (
-              <tr key={i} className="border-b border-white/10 hover:bg-white/5">
+              <TableRow key={i}>
                 <TableCell>
                   <input
                     type="checkbox"
@@ -67,7 +68,7 @@ export function AttendeeList() {
                     <MoreHorizontal className="size-4" />
                   </IconButton>
                 </TableCell>
-              </tr>
+              </TableRow>
             );
           })}
         </tbody>
